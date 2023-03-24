1bd91423cbae7dc0614295a6e56680b4fe6b8069
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-25 03:47:03 +0900

    set an event listner to the button which sets rows and columns and make a function to take user input

---
+++ app.js
@@ -0,0 +1,24 @@
+let rowColumn = 16;    // rows and coulms is 16 by default
+const setRowColumn = document.querySelector('.set');
+
+// setting up the button which prompts the user to input the amount of rows and colmns in the etch a sketch grid 
+setRowColumn.addEventListener("click", rowColumnSetter); 
+
+function rowColumnSetter() {
+  let tempRowCol = prompt("Set rows and columns", 16);
+  if (isNaN(tempRowCol)) {
+    alert("Please enter a number");
+  } else {
+    tempRowCol = Number(tempRowCol);
+    if (tempRowCol <= 0) {
+      alert("Please set a number over zero");
+    } else if (tempRowCol > 100) {
+      alert("Please set a number less than 100");
+    } else {
+      rowColumn = tempRowCol;
+      return tempRowCol;
+    }
+  }
+}
+
+
