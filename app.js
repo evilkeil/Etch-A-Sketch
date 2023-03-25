1264dc0a3087d7bd7da8e4e3f14a3cead9c6336e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-25 15:59:59 +0900

    test the fagment method as a potential way to create the divs (will delete/edit after test)

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ let rowColumn = 16;    // rows and colums is 16 by default
 const setRowColumn = document.querySelector('.set');
 
 // setting up the button which prompts the user to input the amount of rows and colmns in the etch a sketch grid 
-setRowColumn.addEventListener("click", rowColumnSetter); 
+setRowColumn.addEventListener("click", rowColumnSetter);
 
+// function for the row column setter
 function rowColumnSetter() {
   let tempRowCol = prompt("Set rows and columns", 16);
   if (isNaN(tempRowCol)) {
@@ -20,5 +21,25 @@ function rowColumnSetter() {
     }
   }
 }
+
+
+const sketchPad = document.querySelector('#sketch');
+
+//testing document fragment 
+
+// Create a new DocumentFragment
+const fragment = document.createDocumentFragment();
+
+// Create some new DOM nodes and append them to the fragment
+const p1 = document.createElement("p");
+p1.textContent = "This is paragraph 1.";
+fragment.appendChild(p1);
+
+const p2 = document.createElement("p");
+p2.textContent = "This is paragraph 2.";
+fragment.appendChild(p2);
+
+// Append the entire fragment to the main document tree
+sketchPad.appendChild(fragment);
 
 
